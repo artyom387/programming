e12298abc89e2b6ea1ba2bb007df6377590c4755
artakk3y <artak.k3.y@tumo.org>
2021-09-11 16:17:26 +0400

    all files changed

---
+++ lesson1/Classes/GrassEater.js
@@ -0,0 +1,95 @@
+class GrassEater extends LivingCreatures{
+    getNewCordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(char) {
+        this.getNewCordinates();
+        return super.chooseCell(char);
+    }
+    mul() {
+        let found = this.chooseCell(0);
+        let exact = random(found)
+
+        if (exact && this.energy > 8) {
+            let x = exact[0];
+            let y = exact[1];
+
+            let eater = new GrassEater(x, y);
+            matrix[y][x] = 2;
+            grassEaterArr.push(eater);
+
+            this.energy = 20;
+        }
+    }
+    eat() {
+        let found = this.chooseCell(1);
+        let exact = random(found)
+
+        if (exact) {
+            this.energy += 5;
+            let x = exact[0];
+            let y = exact[1];
+
+            for (let i = 0; i < grassArr.length; i++) {
+                if (grassArr[i].x == x && grassArr[i].y == y) {
+                    grassArr.splice(i, 1)
+                }
+            }
+
+            matrix[y][x] = 2
+            matrix[this.y][this.x] = 0
+
+            this.x = x;
+            this.y = y
+
+            if (this.energy > 30) {
+                this.mul()
+            }
+        } else {
+            this.move()
+        }
+    }
+    move() {
+        let found = this.chooseCell(0);
+        let exact = random(found)
+
+        if (exact) {
+            let x = exact[0];
+            let y = exact[1];
+
+            matrix[y][x] = 2
+            matrix[this.y][this.x] = 0
+
+            this.x = x;
+            this.y = y;
+
+            this.energy--
+
+            if (this.energy < 0) {
+                this.die()
+            }
+        } else {
+            this.energy--
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+    die() {
+        for (let i = 0; i < grassEaterArr.length; i++) {
+            if (grassEaterArr[i].x == this.x && grassEaterArr[i].y == this.y) {
+                grassEaterArr.splice(i, 1)
+            }
+        }
+        matrix[this.y][this.x] = 0
+    }
+}
